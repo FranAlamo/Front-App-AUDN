f04760e6434f0653d1374729e01bfc30a94aa7c2
FranAlamo <alamofrancis@gmail.com>
2023-05-08 15:49:28 -0300

    creacion de buscador

--- src/Pages/buscador/Buscador.jsx
+++ src/Pages/buscador/Buscador.jsx
@@ -1,12 +1,61 @@
-import React from 'react'
-import NavBar from '../../components/navbarcomponent/NavBar'
+import React from "react";
+import NavBar from "../../components/navbarcomponent/NavBar";
+import "../buscador/buscador.css";
+import BotonDeRegistro from "../../components/Boton/BotonRegistro";
+import { useState } from "react";
+import dataTop20 from "../../components/dataTop20";
 
 function Buscador() {
-    return (
-        <div>Buscador
-            <NavBar activeImage={"imagen2"} />
-        </div>
-    )
+  const [dataTop20Todos, setdataTop20Todos] = useState(dataTop20);
+
+  const mostrarTodo = () => {
+    setdataTop20Todos(dataTop20);
+  };
+
+  return (
+    <div className="titulo">
+      <section className="buscadorTop">
+        <h1>Buscador</h1>
+        <BotonDeRegistro />
+
+        <h3 style={{ display: "inline-block" }}>Top 20s</h3>
+        <hr
+          style={{
+            display: "inline-block",
+            marginLeft: "10px",
+            height: "1px",
+            width: "240px",
+            border: "none",
+            backgroundColor: "#E4E6E8",
+          }}
+        />
+      </section>
+
+      <div className="main">
+        {dataTop20Todos.map((data) => (
+          <Card key={data.id} data={data} mostrarTodo={mostrarTodo} />
+        ))}
+      </div>
+      <NavBar activeImage={"imagen2"} />
+    </div>
+  );
 }
 
-export default Buscador
+function Card(props) {
+  const { data, mostrarTodo } = props;
+
+  return (
+    <div className="card" onClick={mostrarTodo}>
+      <img
+        src={data.image}
+        alt={data.artista}
+        style={{ width: "152px", height: "152px" }}
+      />
+
+      <h4 className="cancion">{data.cancion}</h4>
+      <h4 className="artista">{data.artista}</h4>
+    </div>
+  );
+}
+
+export default Buscador;
---
+++ src/components/dataTop20.jsx
@@ -0,0 +1,137 @@
+import chachoRamos from "../assets/imagenes/cumbia/chachoRamos.jpg";
+import laConga from "../assets/imagenes/cumbia/laConga.jpg";
+import braulioLopez from "../assets/imagenes/folklore/braulioLopez.jpg";
+import maciegas from "../assets/imagenes/folklore/maciegas.jpg";
+import amy from "../assets/imagenes/Jazz/amy.jpg";
+import aretha from "../assets/imagenes/Jazz/aretha.jpg";
+import louis from "../assets/imagenes/Jazz/louis.jpg";
+import nina from "../assets/imagenes/Jazz/nina.jpg";
+import jBalvin from "../assets/imagenes/regueton/jBalvin.jpg";
+import ozuna from "../assets/imagenes/regueton/ozuna.jpg";
+import guns from "../assets/imagenes/Rock/guns.jpg";
+import theDoors from "../assets/imagenes/Rock/theDoors.jpg";
+import theEagles from "../assets/imagenes/Rock/theEagles.jpg";
+
+const dataTop20 = [
+  {
+    id: "1",
+    cancion: "Boleto al Amor",
+    artista: "Chacho Ramos",
+    image: chachoRamos,
+  },
+  {
+    id: "2",
+    cancion: "Hechicera",
+    artista: "La Conga",
+    image: laConga,
+  },
+  {
+    id: "3",
+    cancion: "A Don José",
+    artista: "Braulio Lopez",
+    image: braulioLopez,
+  },
+  {
+    id: "4",
+    cancion: "hola",
+    artista: "Maciegas",
+    image: maciegas,
+  },
+  {
+    id: "5",
+    cancion: "Back to Black",
+    artista: "Amy Winehouse",
+    image: amy,
+  },
+  {
+    id: "6",
+    cancion: "Respect",
+    artista: "Aretha Franklin",
+    image: aretha,
+  },
+  {
+    id: "7",
+    cancion: "What a Wonderful World",
+    artista: "Louis Armstrong",
+    image: louis,
+  },
+  {
+    id: "8",
+    cancion: "Feeling Good",
+    artista: "Nina Simone",
+    image: nina,
+  },
+  {
+    id: "9",
+    cancion: "Mi Cama",
+    artista: "J Balvin",
+    image: jBalvin,
+  },
+  {
+    id: "10",
+    cancion: "100",
+    artista: "Ozuna",
+    image: ozuna,
+  },
+  {
+    id: "11",
+    cancion: "Sweet Child O' Mine",
+    artista: "Guns and Roses",
+    image: guns,
+  },
+  {
+    id: "12",
+    cancion: "(I Can't Get No) Satisfaction",
+    artista: "The doors",
+    image: theDoors,
+  },
+  {
+    id: "13",
+    cancion: "hola",
+    artista: "The Eagles",
+    image: theEagles,
+  },
+  {
+    id: "14",
+    cancion: "hola",
+    artista: "The Eagles",
+    image: theEagles,
+  },
+  {
+    id: "15",
+    cancion: "hola",
+    artista: "The Eagles",
+    image: theEagles,
+  },
+  {
+    id: "16",
+    cancion: "hola",
+    artista: "The Eagles",
+    image: theEagles,
+  },
+  {
+    id: "17",
+    cancion: "hola",
+    artista: "The Eagles",
+    image: theEagles,
+  },
+  {
+    id: "18",
+    cancion: "hola",
+    artista: "The Eagles",
+    image: theEagles,
+  },
+  {
+    id: "19",
+    cancion: "hola",
+    artista: "The Eagles",
+    image: theEagles,
+  },
+  {
+    id: "20",
+    cancion: "hola",
+    artista: "The Eagles",
+    image: theEagles,
+  },
+];
+export default dataTop20;
