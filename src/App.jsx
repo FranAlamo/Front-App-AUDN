3a8c453d44f5b38cd719d2707e284a2f6887446e
valen2996 <valentinabagnasco7@gmail.com>
2023-05-08 15:53:35 -0300

    body home

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import { useState } from "react";
 import "./App.css";
 import HeaderInicio from "./components/HeaderInicio/HeaderInicio";
 import NavBar from "./components/navbarcomponent/NavBar";
-import BotonRegistro from "./components/Boton/BotonRegistro";
+import Home from "./Pages/home/Home";
 
 function App() {
   return (
     <div className="app">
       <HeaderInicio />
-      <BotonRegistro />
-
+      <Home></Home>
       <NavBar />
     </div>
   );
--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import HeaderInicio from '../../components/HeaderInicio/HeaderInicio';
 import NavBar from '../../components/navbarcomponent/NavBar';
+import BodyHome from '../../components/BodyHome/BodyHome';
+
 
 function Home() {
     return (
         <div>
             <HeaderInicio/>
+            <BodyHome></BodyHome>
             <NavBar activeImage={"imagen1"}/>
         </div>
     )
