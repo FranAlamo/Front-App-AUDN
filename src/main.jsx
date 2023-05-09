fd78caa4d095450067b4bc8bb51543e1b3f5110e
FranAlamo <alamofrancis@gmail.com>
2023-05-09 11:18:22 -0300

    musica contextual

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Home from './Pages/home/Home.jsx'
-import Buscador from './Pages/buscador/Buscador.jsx'
-import Perfil from './Pages/perfil/Perfil.jsx'
-import Configuracion from './Pages/configuracion/Configuracion.jsx'
-import Inicio from './Pages/inicio/Inicio.jsx'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Pages/home/Home.jsx";
+import Buscador from "./Pages/buscador/Buscador.jsx";
+import Perfil from "./Pages/perfil/Perfil.jsx";
+import Configuracion from "./Pages/configuracion/Configuracion.jsx";
+import Inicio from "./Pages/inicio/Inicio.jsx";
+import MusicaContextual from "./Pages/musicaContextual/musicaContextual.jsx";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Inicio/>},
+  { path: "/", element: <Inicio /> },
   { path: "/home", element: <Home /> },
   { path: "/buscador", element: <Buscador /> },
   { path: "/perfil", element: <Perfil /> },
-  { path: "/configuracion", element: <Configuracion /> }
-])
+  { path: "/configuracion", element: <Configuracion /> },
+  { path: "/musicacontextual", element: <MusicaContextual /> },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
