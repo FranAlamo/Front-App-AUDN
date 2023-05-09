d7448edc453591f9f55e9291320a2c36d6c57200
FranAlamo <alamofrancis@gmail.com>
2023-05-09 15:41:58 -0300

    modificacion data y buscador

--- src/Pages/musicaContextual/musicaContextual.jsx
+++ src/Pages/musicaContextual/musicaContextual.jsx
@@ -1,45 +1,50 @@
 import React from "react";
 import "../musicaContextual/musicaContextual.css";
 import BotonRegistro from "../../components/Boton/botonRegistro";
+import GeneralHeader from "../../components/generalheader/GeneralHeader";
+import InputComponent from "../../components/input/input";
 function contextual() {
   return (
     <div className="contenedorContextual">
-      <h3 className="tituloMusicaContextual">Música Contextual</h3>
+      <GeneralHeader link="/home" title="Música Contextual" />
+
       <div className="preguntasContextual">
         <section className="ocasion">¿Cuál es la ocasión?</section>
-        <BotonRegistro txt="Actividad" />
+        <InputComponent bgcolor="musicaContextualBordes" />
 
         <section className="sientes">¿Cómo te sientes?</section>
-        <BotonRegistro txt="Estado de ánimo" />
+        <InputComponent />
 
         <section className="clima">¿Cómo está el clima?</section>
-        <BotonRegistro txt="Clima" />
+        <InputComponent />
       </div>
       <div className="seleccionContextual">
         <h1 className="generos">Selecciona hasta 3 géneros</h1>
-
-        <BotonRegistro txt="Clásico" bgcolor="botonAjustable" />
-        <BotonRegistro txt="Electrónica" bgcolor="botonAjustable" />
+      </div>
+      <div className="todosGeneros1">
+        <BotonRegistro txt="Rock" bgcolor="botonAjustable" />
         <BotonRegistro txt="Country" bgcolor="botonAjustable" />
         <BotonRegistro txt="Soul" bgcolor="botonAjustable" />
+        <BotonRegistro txt="Jazz" bgcolor="botonAjustable" />
         <BotonRegistro txt="Blues" bgcolor="botonAjustable" />
-        <BotonRegistro txt="Hip-Hop" bgcolor="botonAjustable" />
+        <BotonRegistro txt="Hip-hop" bgcolor="botonAjustable" />
         <BotonRegistro txt="Pop" bgcolor="botonAjustable" />
         <BotonRegistro txt="Reggae" bgcolor="botonAjustable" />
         <BotonRegistro txt="Folk" bgcolor="botonAjustable" />
         <BotonRegistro txt="R&B" bgcolor="botonAjustable" />
+        <BotonRegistro txt="Clásico" bgcolor="botonAjustable" />
         <BotonRegistro txt="Alternativo" bgcolor="botonAjustable" />
         <BotonRegistro txt="Ambiente" bgcolor="botonAjustable" />
         <BotonRegistro txt="EDM" bgcolor="botonAjustable" />
-        <BotonRegistro txt="New Age" bgcolor="botonAjustable" />
-        <BotonRegistro txt="Jazz" bgcolor="botonAjustable" />
-        <BotonRegistro txt="Disco" bgcolor="botonAjustable" />
-        <BotonRegistro txt="Punk" bgcolor="botonAjustable" />
-        <BotonRegistro txt="Rock" bgcolor="botonAjustable" />
+        <div className="todosGeneros2">
+          <BotonRegistro txt="Electrónica" bgcolor="botonAjustable" />
+          <BotonRegistro txt="Disco" bgcolor="botonAjustable" />
+          <BotonRegistro txt="New Age" bgcolor="botonAjustable" />
+          <BotonRegistro txt="Punk" bgcolor="botonAjustable" />
+        </div>
       </div>
-
       <div>
-        <BotonRegistro className="crearPlayist" txt="Crear playist" />
+        <BotonRegistro bgcolor="crearPlaylist" txt="Crear playist" />
       </div>
     </div>
   );
--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import './Input.css';
+import React from "react";
+import "./Input.css";
 
 function InputComponent() {
   return (
-    <div><input className='inputComponent'></input>
+    <div>
+      <input className="inputComponent"></input>
     </div>
-  )
+  );
 }
 
 export default InputComponent;
