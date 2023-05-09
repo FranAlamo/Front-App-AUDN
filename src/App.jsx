434b9152e08c6e70bb0dba6c22a7243c8326b248
valen2996 <valentinabagnasco7@gmail.com>
2023-05-08 21:06:40 -0300

    cambios bodyhome y header

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { useState } from "react";
 import "./App.css";
 import HeaderInicio from "./components/HeaderInicio/HeaderInicio";
 import NavBar from "./components/navbarcomponent/NavBar";
-
-
+import Home from "./Pages/home/Home"
 
 function App() {
   return (
