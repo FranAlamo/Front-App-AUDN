17c517077405af609ba721a2c58ba9ae307526ef
Deividds1 <deivid1421@gmail.com>
2023-05-10 13:22:59 -0300

    busqueda

--- src/Pages/busquedas/Busquedas.jsx
+++ src/Pages/busquedas/Busquedas.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import InputComponent from '../../components/input/input.jsx'
+import '../busquedas/busquedas.css';
+import teclado from '../../assets/imagenes/teclado/keyboard.svg';
+
 
 function Busquedas() {
     let bgcolor = '';
 
     return (
         <div className='busquedas-container'>
-            <InputComponent bgcolor='buscador' />
+            <InputComponent bgcolor='buscadorGris' />
+            <div className='title-container'>
+                <p className="first-p">Búsquedas Recientes:</p>
+                <div className="busqueda-bar"></div>
+            </div>
+
+            <div className="lista-busquedas"></div>
+
+            <div className='teclado-container'>
+                <p className="second-p">Borrar búsquedas Recientes:</p>
+                <img className='teclado-busqueda' src={teclado} alt="teclado" />
+            </div>
         </div>
     )
 }
