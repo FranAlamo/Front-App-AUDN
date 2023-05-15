f4c09a31845132689baa67fbb757c126b3a1093f
FranAlamo <alamofrancis@gmail.com>
2023-05-15 13:57:43 -0300

    creacion de playist desde la base de datos

--- src/Pages/playlist/Playlist.jsx
+++ src/Pages/playlist/Playlist.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import GeneralHeader from "../../components/generalheader/GeneralHeader";
 import NavBar from "../../components/navbarcomponent/NavBar";
 import "./playlist.css";
@@ -10,17 +11,64 @@ import copia from "../../assets/imagenes/playlist-images/copia.png";
 import play from "../../assets/imagenes/playlist-images/play.png";
 import aleatorio from "../../assets/icons/state=active-1.svg";
 
-
 function Playlist() {
+  const { state } = useLocation();
+  const [generos, setGeneros] = useState(
+    state.generosActivos ? state.generosActivos : []
+  );
+  const [canciones, setCanciones] = useState([]);
+  const [cancionesFotos, setCancionesFotos] = useState([]);
+
+  const cargarMusicaPorGenero = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", localStorage.getItem("token"));
+    myHeaders.append("Content-Type", "application/json");
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+    try {
+      const response = await fetch(
+        `http://localhost:8000/api/porGenero/${generos[0]}`,
+        requestOptions
+      ).then((response) => {
+        return response;
+      });
+
+      if (response.ok) {
+        const respuesta = await response.json();
+        setCanciones(respuesta.musica);
+        if (respuesta.musica.length > 4) {
+          setCancionesFotos(respuesta.musica.splice(0, 4));
+        } else {
+          setCancionesFotos(respuesta.musica);
+        }
+      } else {
+        alert("Ocurrio un error del lado del cliente");
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (generos.length) cargarMusicaPorGenero();
+  }, [generos]);
+
   return (
     <div>
       <header>
         <GeneralHeader title="Playlist Generada"> </GeneralHeader>
       </header>
       <div className="playlist-imagen">
-    <div className="img-song">
-        
-    </div>
+        <div className="img-song">
+          {cancionesFotos.map((cancion) => (
+            <div>
+              <img src={`../../imagenes${cancion.imagen}`} />
+            </div>
+          ))}
+        </div>
       </div>
       <section className="playlist-minutos">
         <div className="playlist-compartir">
@@ -45,15 +93,17 @@ function Playlist() {
       </section>
       <section className="playlist-container">
         <div className="playlist-container_song">
-          <div>
-           
-          </div>
+          <div></div>
           <div className="songs-container">
-           
+            {canciones.map((cancion) => (
+              <div>
+                <img src={`../../imagenes${cancion.imagen}`} />
+                <span>{cancion.artista}</span>
+                <span>{cancion.nombre_genero}</span>
+              </div>
+            ))}
           </div>
         </div>
-
-       
       </section>
       <div>
         <NavBar></NavBar>
